8a5917d9f6b0e32ca08e7521ef92a2b4397d8b0b
= <=>
2023-10-22 15:56:13 +0800

    Finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://example.com"
+  site: "https://6534d2f657ebb940879f6ed5--vermillion-treacle-099dd9.netlify.app/",
+  integrations: [preact()]
 });
